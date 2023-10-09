fb70f8c0e01686478054f57c969faf32cc430d45
Kaushik Mahindra Kar <67312877+kaushik18VK@users.noreply.github.com>
2023-10-09 15:08:10 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+// import { useState } from 'react';
+import './App.css';
+import About from './component/About';
+import Navbar from './component/Navbar';
+import TextForm from './component/txtForm';
+import React, { useState } from 'react';
+import Alert from './component/Alertt';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+
+function App() {
+  const [mode, setMode]= useState('light');
+  const [alert,setAlert]=useState(null)
+  
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type,
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#042743';
+      showAlert('Dark mode is enabled','success');
+      // document.title='TextUtils - Dark Mode'
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert('Dark mode is disabled','success');
+      // document.title='TextUtils - Light Mode'
+    }
+  }
+  return (
+    <>
+      <BrowserRouter>
+    <Navbar title="Wordz Play" aboutText="About" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert= {alert}/>
+    <br />
+    <div className="container my-3">
+      <Routes>
+        {/* <About/> */}
+      <Route exact path="/about" element={<About mode={mode} />} />
+      </Routes>
+      <Routes>
+        {/* <TextForm showAlert={showAlert} heading="Try Wordz Play - Text Converter, Word counter, Character counter"mode={mode} /> */}
+        <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try Wordz Play - Text Converter, Word counter, Character counter"mode={mode} />}></Route>
+    </Routes>
+    </div>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
+ 
---
+++ src/component/txtForm.js
@@ -0,0 +1,71 @@
+import React ,{useState} from 'react'
+
+export default function TxtForm(props) {
+  const handleUpclick=()=>{
+    let newtext=text.toUpperCase()
+    setText(newtext)
+    props.showAlert("Converted to upper case!","success")
+  }
+
+    const handleDoclick=()=>{
+      let newtext=text.toLowerCase()
+      setText(newtext)
+      props.showAlert("Converted to lower case!","success")
+    }
+    const handleClearclick=()=>{
+      let newtext=""
+      setText(newtext)
+      props.showAlert("Cleared text!","success")
+    }
+    const handlerevlick=()=>{
+      let newtext=text.split(" ").reverse().join(" ")
+      setText(newtext)
+      props.showAlert("Reveresed string!","success")
+    }
+    const handleCopy=()=>{
+      var text=document.getElementById("Mybox");
+      text.select();
+      navigator.clipboard.writeText(text.value);
+      document.getSelection().removeAllRanges();
+      props.showAlert("Copied Text!","success")
+    }
+    const titleCase = () => {
+      const words = text.split(' ');
+      const newWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+      const newText = newWords.join(' ');
+      setText(newText);
+      props.showAlert('Converted to title Case!', 'success');
+    };
+    
+    
+const HandleonChange=(event)=>{
+  setText(event.target.value)
+}
+
+  const [text, setText] = useState('');
+  return (
+   <>
+   <div className='container' style={{color:props.mode==='dark'?'white':'black'}}>
+      <h1 className='mb-2'>{props.heading}</h1>
+<div className="mb-3">
+  <textarea className="form-control" value={text} id="Mybox" rows="8" onChange={HandleonChange} style={{backgroundColor:props.mode==='dark'?'#13466e':'white',color:props.mode==='dark'?'white':'black'}} ></textarea>
+</div>
+<button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpclick}>Convert to Uppercase</button>
+<button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleDoclick}>Convert to Lowercase</button>
+<button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearclick}>Cleartext</button>
+<button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handlerevlick}>Reverse words</button>
+<button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>copy text</button>
+<button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={titleCase}>Title case</button>
+
+
+    </div>
+    <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
+      <h2>Your summary is here</h2>
+      <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} and {text.length} </p>
+      <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes to read</p>
+      <h2>Preview</h2>
+      <p>{text.length>0? text:"Nothing to preview!"} </p>
+    </div>
+    </>
+  )
+}
